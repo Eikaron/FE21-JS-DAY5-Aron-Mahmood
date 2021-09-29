4162abe18b2c56fd22399e411bcc6204d276e6bd
mahmoodalqaisi232 <memoqaisi232@gmail.com>
2021-09-29 15:07:01 +0200

    some js

--- Extra/extra.js
+++ Extra/extra.js
@@ -5,16 +5,22 @@ class Hotel {
     numberOfNights;
     address;
     image;
-        constructor(h,n,a,i){
+        constructor(h,a,i){
             this.hotelName=h;
             this.numberOfNights=Math.floor(Math.random()*9 + 1);
             this.address=a;
             this.image=i;
         }
-        showAvailability(){
-            
+        showAvailability(nights){
 
+            if(this.numberOfNights>nights){
+                return  <button value="I'll reserve"></button>
+
+            }else{
+                return  `<button value="Sorry, no available nights for ${hotelName}></button> `
+
+            }
         }
-
-
 }
+
+Hotel1= new Hotel("holdayInn","Vienna",)
